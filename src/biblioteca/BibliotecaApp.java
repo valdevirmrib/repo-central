00c3877ba3989449836cccbbd8eecb791d75acff
Valdevir <valdevirmrib@gmail.com>
2021-03-19 16:28:14 -0300

    Revert "criada a classe emprestimo."
    This reverts commit 408afa05dbe9b24291728c8fef043709804099db.

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -5,10 +5,7 @@ public class BibliotecaApp {
 	public static void main(String[] args) {
 		
 		Livro livro = new Livro();
-		Usuario usuario = new Usuario();
 		
-		Emprestimo emprestimo = new Emprestimo();
-				
 		
 	}
 
