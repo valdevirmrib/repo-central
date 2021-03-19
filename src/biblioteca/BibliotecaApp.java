408afa05dbe9b24291728c8fef043709804099db
Valdevir <valdevirmrib@gmail.com>
2021-03-19 16:26:28 -0300

    criada a classe emprestimo.

--- src/biblioteca/BibliotecaApp.java
+++ src/biblioteca/BibliotecaApp.java
@@ -5,7 +5,10 @@ public class BibliotecaApp {
 	public static void main(String[] args) {
 		
 		Livro livro = new Livro();
+		Usuario usuario = new Usuario();
 		
+		Emprestimo emprestimo = new Emprestimo();
+				
 		
 	}
 
---
+++ src/biblioteca/Emprestimo.java
@@ -0,0 +1,9 @@
+package biblioteca;
+
+public class Emprestimo {
+	
+	private Livro livro;
+	private Usuario usuario;
+	
+
+}
